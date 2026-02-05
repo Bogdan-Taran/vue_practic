58bf7325e9248e9507a64e8b8cbc2f17a3c26e47
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-02-05 11:45:37 +0700

    I have learned how to make computed properties and use it in UI, made the 7th practical work

--- vue/js/main.js
+++ vue/js/main.js
@@ -3,32 +3,52 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        // image: "./assets/vmSocks-green-onWhite.jpg",
         // image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: 'A pair of socks',
-        inStock: true,
+        // inStock: true,
+        onSale: true,
         details: ['80% cotton', '20% polyster', 'Gender-neutral'],
         variants: [
             {
                 variantId: 1111,
                 variantColor: 'green',
                 variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10,
             },
             {
                 variantId: 1112,
                 variantColor: 'blue',
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0,
             }
         ],
         cart: 0,
     },
     methods: {
-        addToCart(){
+        addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index)
         }
     },
-    
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        isOnSale() {
+            return this.onSale ? 'Распродажа для ' + this.title : 'Распродажи нет'
+        }
+    }
+
 })
