dcd67d9ed4ab25790cbd89deb7e425515e83c913
Bogdan-Taran <taranbogdan9.2@mail.ru>
2026-02-13 02:46:10 +0700

    made blocking the first column if the second contains 5 notes

--- vue/js/main.js
+++ vue/js/main.js
@@ -4,11 +4,11 @@ let eventBus = new Vue()
 Vue.component('table-cards', {
     template: `
     <div class="notes-table">
-        <div class="notes-table-container column1">
+        <div class="notes-table-container column1" :class="{locked: firstColumnLocked}">
             <h2>{{columnsNotes[0].title}}</h2>
             <div>
                 <li v-for="(note, index) in columnsNotes[0].notes" :key="index">
-                    <note-card :noteDetail="note" :columnIndex="0" @move-to-column="handleMoveToColumn"></note-card>
+                    <note-card :noteDetail="note" :columnIndex="0" :isLocked="firstColumnLocked" @move-to-column="handleMoveToColumn" @percentage-changed="checkFirstColumnLock"></note-card>
                 </li>
             </div>
         </div>
@@ -36,7 +36,8 @@ Vue.component('table-cards', {
                 {title: 'Column 1', notes: []},
                 {title: 'Column 2', notes: []},
                 {title: 'Column 3', notes: []}
-            ]
+            ],
+            firstColumnLocked: false,
         }
     },
     mounted() {
@@ -52,6 +53,37 @@ Vue.component('table-cards', {
             if(currentColumnIndex === -1 || currentColumnIndex === nextColumnIndex) return
             this.columnsNotes[currentColumnIndex].notes.splice(this.columnsNotes[currentColumnIndex].notes.indexOf(note), 1)
             this.columnsNotes[nextColumnIndex].notes.push(note)
+            if(currentColumnIndex === 1 && nextColumnIndex === 2){
+                this.checkUnlockFirstColumn()
+            }
+        },
+        checkFirstColumnLock(){
+            if(this.columnsNotes[1].notes.length >= 5 && !this.firstColumnLocked){
+                const shouldLock = this.columnsNotes[0].notes.some(note => {
+                    const items = ['item1', 'item2', 'item3', 'item4', 'item5']
+                    let total = 0
+                    let completed = 0
+                    items.forEach(itemKey => {
+                        if(note[itemKey] !== undefined && note[itemKey] !== null){
+                            total++
+                            const checkedKey = `${itemKey}Checked`
+                            if(note[checkedKey]){
+                                completed++
+                            }
+                        }
+                    })
+                    const percentage = total ? Math.round((completed / total) * 100) : 0
+                    return percentage > 50
+                })
+                if(shouldLock){
+                    this.firstColumnLocked = true
+                }
+            }
+        },
+        checkUnlockFirstColumn(){
+            if(this.firstColumnLocked && this.columnsNotes[1].notes.length < 5){
+                this.firstColumnLocked = false
+            }
         }
     }
 })
@@ -63,29 +95,30 @@ Vue.component('note-card', {
             required: false,
         },
         columnIndex: Number,
+        isLocked: Boolean,
     },
     template: `
     <div class="note">
         <ul class="note-list">
             <h3>{{noteDetail.title}}</h3>
             <li :class="{completed: noteDetail.item1Checked}">
-                <input type="checkbox" :id="'note-item1-'+ _uid" v-model="noteDetail.item1Checked">
+                <input type="checkbox" :id="'note-item1-'+ _uid" v-model="noteDetail.item1Checked" :disabled="isLocked">
                 <label :for="'note-item1-'+ _uid">{{noteDetail.item1}}</label>
             </li>
             <li :class="{completed: noteDetail.item2Checked}">
-                <input type="checkbox" :id="'note-item2-'+ _uid" v-model="noteDetail.item2Checked">
+                <input type="checkbox" :id="'note-item2-'+ _uid" v-model="noteDetail.item2Checked" :disabled="isLocked">
                 <label :for="'note-item2-'+ _uid">{{noteDetail.item2}}</label>
             </li>
             <li :class="{completed: noteDetail.item3Checked}">
-                <input type="checkbox" :id="'note-item3-'+ _uid" v-model="noteDetail.item3Checked">
+                <input type="checkbox" :id="'note-item3-'+ _uid" v-model="noteDetail.item3Checked" :disabled="isLocked">
                 <label :for="'note-item3-'+ _uid">{{noteDetail.item3}}</label>
             </li>
             <li v-if="noteDetail.item4" :class="{completed: noteDetail.item4Checked}">
-                <input type="checkbox" :id="'note-item4-'+ _uid" v-model="noteDetail.item4Checked">
+                <input type="checkbox" :id="'note-item4-'+ _uid" v-model="noteDetail.item4Checked" :disabled="isLocked">
                 <label :for="'note-item4-'+ _uid">{{noteDetail.item4}}</label>
             </li>
             <li v-if="noteDetail.item5" :class="{completed: noteDetail.item5Checked}">
-                <input type="checkbox" :id="'note-item5-'+ _uid" v-model="noteDetail.item5Checked">
+                <input type="checkbox" :id="'note-item5-'+ _uid" v-model="noteDetail.item5Checked" :disabled="isLocked">
                 <label :for="'note-item5-'+ _uid">{{noteDetail.item5}}</label>
             </li>
             <p class="progress-text">Completed: {{completionPercentage}}% </p>
@@ -119,6 +152,7 @@ Vue.component('note-card', {
     },
     watch: {
         completionPercentage(newVal){
+            this.$emit('percentage-changed')
             if(this.columnIndex === 0){
                 if(newVal > 50){
                     this.$emit('move-to-column', this.noteDetail, 1)
